b1af22f8407fb4b59fcceda9e54b24d6581b6dcd
Shubh Rajawat <shubh.rajawat@parasmesoft.com>
2026-01-07 16:20:26 +0530

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ import typography from '@tailwindcss/typography'
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1920px',
+      },
+    },
   },
   plugins: [typography],
 }
