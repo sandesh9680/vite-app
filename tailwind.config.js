5fd6ebd7779f14be5b662ed9c899414dbddeaea2
Shubh Rajawat <shubh.rajawat@parasmesoft.com>
2026-01-07 15:33:01 +0530

    reverting changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,7 @@ export default {
   ],
   theme: {
     extend: {},
-    container: {
-      center: true,
-      screens: {
-        '2xl': '1920px',
-      },
-    },
+    
   },
   plugins: [],
 }
